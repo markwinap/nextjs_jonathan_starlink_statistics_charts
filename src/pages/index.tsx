6f86801612e03db503c3ae4e8d3973de7f6ef9c3
markwinap <markwinap@gmail.com>
2024-07-12 20:10:06 -0600

    Update packaged and change charts lib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,86 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head'
-import Image from 'next/image'
+import Script from 'next/script'
 import { parseDocument } from 'htmlparser2';
 import { getAttributeValue, findAll, textContent } from 'domutils';
-import dayjs from 'dayjs';
-import zoomPlugin from 'chartjs-plugin-zoom';
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from 'chart.js';
-import { Bar } from 'react-chartjs-2';
-import { Inter } from 'next/font/google'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
-const inter = Inter({ subsets: ['latin'] })
-const isBrowser = () => typeof window !== 'undefined';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-);
 interface IStats {
   mission: string;
-  total_sats: string;
-  early_deorbit: string;
-  disposal_complete: string;
-  reentry_after_fail: string;
-  total_in_orbit: string;
-  screened: string;
-  failed_decaying: string;
-  graveyard: string;
-  total_working: string;
-  disposal_underway: string;
-  out_of_constellation: string;
-  anomaly: string;
-  reserve_relocating: string;
-  special: string;
-  drift: string;
-  ascent: string;
-  operational: string;
+  total_sats: number;
+  early_deorbit: number;
+  disposal_complete: number;
+  reentry_after_fail: number;
+  total_in_orbit: number;
+  screened: number;
+  failed_decaying: number;
+  graveyard: number;
+  total_working: number;
+  disposal_underway: number;
+  out_of_constellation: number;
+  anomaly: number;
+  reserve_relocating: number;
+  special: number;
+  drift: number;
+  ascent: number;
+  operational: number;
+  total_operational: number;
   orbit_heights: string;
   phase_vs_plane: string;
   planevs_time: string;
-}
-const options = {
-  plugins: {
-    title: {
-      display: true,
-      text: 'Starlink Launch Statistics',
-    },
-
-  },
-  responsive: true,
- // maintainAspectRatio: false,
-  interaction: {
-    mode: 'index' as const,
-    intersect: true,
-  },
-  scales: {
-    x: {
-      stacked: true,
-      ticks: {
-        font: {
-          size: 10,
-          autoSkip: false,
-          maxRotation: 0,
-          minRotation: 0
-        }
-      }
-    },
-    y: {
-      stacked: true,
-    },
-  },
 };
+interface ChartLabel {
+  name: string;
+  color: string;
+}
 
 const fetchHtml = async () => {
   const response = await fetch('https://planet4589.org/space/con/star/stats.html');
@@ -103,7 +55,7 @@ const parseHtml = (document: any): IStats[] => {
   }, document.children);
   // console.log(tableElements);
   const lastTableElement = tableElements.pop() || { children: [] };
-  console.log(lastTableElement);
+  // console.log(lastTableElement);
 
   const trElements = findAll((e) => {
     return e.type === 'tag' && e.name === 'tr';
@@ -135,29 +87,30 @@ const parseHtml = (document: any): IStats[] => {
       const drift = textContent(tdElements[17]).trim();
       const ascent = textContent(tdElements[18]).trim();
       const operational = textContent(tdElements[19]).trim();
-      const orbit_heights =  getImgUrl(tdElements[20]?.lastChild)
+      const orbit_heights = getImgUrl(tdElements[20]?.lastChild)
       const phase_vs_plane = getImgUrl(tdElements[21]?.lastChild)
       const planevs_time = getImgUrl(tdElements[22]?.lastChild)
 
       res.push({
         mission,
-        total_sats,
-        early_deorbit,
-        disposal_complete,
-        reentry_after_fail,
-        total_in_orbit,
-        screened,
-        failed_decaying,
-        graveyard,
-        total_working,
-        disposal_underway,
-        out_of_constellation,
-        anomaly,
-        reserve_relocating,
-        special,
-        drift,
-        ascent,
-        operational,
+        total_sats: parseInt(total_sats, 0),
+        early_deorbit: parseInt(early_deorbit, 0),
+        disposal_complete: parseInt(disposal_complete, 0),
+        reentry_after_fail: parseInt(reentry_after_fail, 0),
+        total_in_orbit: parseInt(total_in_orbit, 0),
+        screened: parseInt(screened, 0),
+        failed_decaying: parseInt(failed_decaying, 0),
+        graveyard: parseInt(graveyard, 0),
+        total_working: parseInt(total_working, 0),
+        disposal_underway: parseInt(disposal_underway, 0),
+        out_of_constellation: parseInt(out_of_constellation, 0),
+        anomaly: parseInt(anomaly, 0),
+        reserve_relocating: parseInt(reserve_relocating, 0),
+        special: parseInt(special, 0),
+        drift: parseInt(drift, 0),
+        ascent: parseInt(ascent, 0),
+        operational: parseInt(operational, 0),
+        total_operational: (parseInt(total_working, 0) - parseInt(disposal_underway, 0) - parseInt(out_of_constellation, 0) - parseInt(anomaly, 0) - parseInt(reserve_relocating, 0) - parseInt(special, 0) - parseInt(drift, 0) - parseInt(ascent, 0)),
         orbit_heights,
         phase_vs_plane,
         planevs_time,
@@ -173,131 +126,64 @@ const getDate = (mission: string) => {
   console.log(date);
 };
 
-export default function Home({ data }: any) {
-  const [labels, setLabels] = useState<string[]>([]);
-  const [datasets, setDatasets] = useState<any[]>([]);
+export default function Home({ data }: { data: IStats[] }) {
+  const [labels, setLabels] = useState<ChartLabel[]>([]);
   useEffect(() => {
-    const _labels = data.map((p: IStats, i: number) => `${p.mission}`);
+
+    const _labels = [
+      { name: 'early_deorbit', color: 'rgb(241, 90, 89)' },
+      { name: 'disposal_complete', color: 'rgb(237, 43, 42)' },
+      { name: 'reentry_after_fail', color: 'rgb(210, 19, 18)' },
+      { name: 'screened', color: 'rgb(231, 70, 70)' },
+      { name: 'failed_decaying', color: 'rgb(252, 79, 0)' },
+      { name: 'disposal_underway', color: 'rgb(252, 79, 0)' },
+      { name: 'out_of_constellation', color: 'rgb(255, 217, 90)' },
+      { name: 'anomaly', color: 'rgb(249, 123, 34)' },
+      { name: 'reserve_relocating', color: 'rgb(8, 131, 149)' },
+      { name: 'special', color: 'rgb(10, 77, 104)' },
+      { name: 'drift', color: 'rgb(157, 192, 139)' },
+      { name: 'ascent', color: 'rgb(199, 232, 202)' },
+      { name: 'total_operational', color: 'rgb(96, 153, 102)' },
+    ];
     setLabels(_labels);
 
-    const _datasets = [
-      // {
-      //   label: 'Total Sats',
-      //   data: data.map((p: IStats) => p.total_sats),
-      //   backgroundColor: 'rgb(155, 164, 181)',
-      //   stack: 'Total',
-      // },
-      {
-        label: 'Early Deorbit',
-        data: data.map((p: IStats) => p.early_deorbit),
-        backgroundColor: 'rgb(241, 90, 89)',
-        stack: 'Group',
-      },
-      {
-        label: 'Disposal Complete',
-        data: data.map((p: IStats) => p.disposal_complete),
-        backgroundColor: 'rgb(237, 43, 42)',
-        stack: 'Group',
-      },
-      {
-        label: 'Reentry After Fail',
-        data: data.map((p: IStats) => p.reentry_after_fail),
-        backgroundColor: 'rgb(210, 19, 18)',
-        stack: 'Group',
-      },
-      // {
-      //   label: 'Total In Orbit',
-      //   data: data.map((p: IStats) => p.total_in_orbit),
-      //   backgroundColor: 'rgb(57, 72, 103)',
-      //   stack: 'Total In Orbit',
-      // },
-      {
-        label: 'Screened',
-        data: data.map((p: IStats) => p.screened),
-        backgroundColor: 'rgb(231, 70, 70)',
-        stack: 'Group',
-      },
-      {
-        label: 'Failed Decaying',
-        data: data.map((p: IStats) => p.failed_decaying),
-        backgroundColor: 'rgb(255, 99, 132)',
-        stack: 'Group',
-      },
-      {
-        label: 'Graveyard',
-        data: data.map((p: IStats) => p.graveyard),
-        backgroundColor: 'rgb(252, 79, 0)',
-        stack: 'Group',
-      },
-      // {
-      //   label: 'Total Working',
-      //   data: data.map((p: IStats) => p.total_working),
-      //   backgroundColor: 'rgb(246, 241, 241)',
-      //   stack: 'Group',
-      // },
-      {
-        label: 'Disposal Underway',
-        data: data.map((p: IStats) => p.disposal_underway),
-        backgroundColor: 'rgb(252, 79, 0)',
-        stack: 'Group',
-      },
-      {
-        label: 'Out of Constellation',
-        data: data.map((p: IStats) => p.out_of_constellation),
-        backgroundColor: 'rgb(255, 217, 90)',
-        stack: 'Group',
-      },
-      {
-        label: 'Anomaly',
-        data: data.map((p: IStats) => p.anomaly),
-        backgroundColor: 'rgb(249, 123, 34)',
-        stack: 'Group',
-      },
-      {
-        label: 'Reserve Relocating',
-        data: data.map((p: IStats) => p.reserve_relocating),
-        backgroundColor: 'rgb(8, 131, 149)',
-        stack: 'Group',
-      },
-      {
-        label: 'Special',
-        data: data.map((p: IStats) => p.special),
-        backgroundColor: 'rgb(10, 77, 104)',
-        stack: 'Group',
-      },
-      {
-        label: 'Drift',
-        data: data.map((p: IStats) => p.drift),
-        backgroundColor: 'rgb(157, 192, 139)',
-        stack: 'Group',
-      },
-      {
-        label: 'Ascent',
-        data: data.map((p: IStats) => p.ascent),
-        backgroundColor: 'rgb(199, 232, 202)',
-        stack: 'Group',
-      },
-      {
-        label: 'Operational',
-        data: data.map((p: IStats) => (parseInt(p.total_working, 0) - parseInt(p.disposal_underway, 0) - parseInt(p.out_of_constellation, 0) - parseInt(p.anomaly, 0) - parseInt(p.reserve_relocating, 0) - parseInt(p.special, 0) - parseInt(p.drift, 0) - parseInt(p.ascent, 0))),
-        backgroundColor: 'rgb(96, 153, 102)',
-        stack: 'Group',
-      },
-    ]
-    setDatasets(_datasets);
+    //setChartData
   }, [data]);
+
   return (
-    <div>
+    <>
       <Head>
         <title>Starlink Launch Statistics</title>
       </Head>
-      <main
-        className={`bg-gray-50 flex overflow-x-auto space-x-8 w-full bg min-h-max flex-col items-center justify-between p-20`}
-      >
-        <p className="text-black text-lg">Source: Jonathan McDowell <a className="text-blue-600 text-lg" href="https://planet4589.org" target="_blank">https://planet4589.org</a></p>
-        <Bar options={options} data={{ labels, datasets }} />
+      <Script src="https://unpkg.com/react/umd/react.production.min.js" />
+      <Script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js" />
+      <Script src="https://unpkg.com/recharts/umd/Recharts.min.js" />
+      <main className={`bg-gray-50 flex overflow-x-auto space-x-8 w-full bg min-h-max flex-col items-center justify-between p-20`}>
+        <p className="text-black text-lg">Source: Jonathan McDowell <a className="text-blue-600 text-md" href="https://planet4589.org" target="_blank">https://planet4589.org</a></p>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="mission" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            {
+              labels.map((label, i) => <Bar key={i} dataKey={label.name} stackId="a" fill={label.color} />)
+            }
+          </BarChart>
+        </ResponsiveContainer>
       </main>
-    </div>
+    </>
   )
 }
 // This gets called on every request
